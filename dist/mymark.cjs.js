5583decf2646703eebd7ed664b7660d1e1220e6c
wenqing <wenqing@kerrylan.com>
2023-07-31 15:34:26 +0800

    feat: refactor rollup to v3

--- dist/mymark.cjs.js
+++ dist/mymark.cjs.js
@@ -1,1 +1,87 @@
-"use strict";Object.defineProperty(exports,"__esModule",{value:!0});function t(t,e,i,o,n){var r=e.getContext("2d");if(r)for(var a=e.width,s=0,d=0,l=0;l<t.length;l++)(s+=r.measureText(t[l]).width)>a-i&&(r.fillText(t.substring(d,l),i,o),o+=n,s=0,d=l),l===t.length-1&&r.fillText(t.substring(d,l+1),i,o)}var e=function(i){var o=i.container,n=void 0===o?"body":o,r=i.width,a=void 0===r?"400px":r,s=i.height,d=void 0===s?"300px":s,l=i.content,c=void 0===l?"mymark":l,v=i.textAlign,u=void 0===v?"center":v,f=i.textBaseline,g=void 0===f?"middle":f,b=i.font,h=void 0===b?"18px Microsoft Yahei":b,p=i.fillStyle,x=void 0===p?"rgba(184, 184, 184, 0.3)":p,m=i.lineHeight,y=void 0===m?25:m,w=i.rotate,A=void 0===w?20:w,S=i.zIndex,_=void 0===S?1024:S,O=i.observe,M=void 0===O||O,k=i.open;if(void 0===k||k){var L="string"==typeof n?document.querySelector(n):n;if(L){var T=document.createElement("canvas");T.setAttribute("width",a),T.setAttribute("height",d);var q=T.getContext("2d");if(q){q.textAlign=u,q.textBaseline=g,q.font=h,q.fillStyle=x,q.rotate(Math.PI/180*A);var B,C,E=parseFloat(a)/2,j=parseFloat(d)/2;"Array"===(B=c,(C=Object.prototype.toString.call(B)).slice(C.indexOf(" ")+1,C.length-1))?c.forEach((function(e,i){t(e,T,E,j+y*i,y)})):t(c,T,E,j,y);var z=T.toDataURL(),F=document.querySelector(".__wm"),I=F||document.createElement("div"),J="\n    position: absolute;\n    top: 0;\n    left: 0;\n    width: 100%;\n    height: 100%; \n    z-index: ".concat(_,";\n    pointer-events: none;\n    background-repeat: repeat;\n    background-image: url('").concat(z,"')");I.setAttribute("style",J),I.classList.add("__wm"),F||(L.style.position="relative",L.insertBefore(I,L.firstChild));var N=window.MutationObserver||window.WebKitMutationObserver;if(M&&N){var P=new N((function(){var t=document.querySelector(".__wm");(t&&t.getAttribute("style")!==J||!t)&&(P.disconnect(),e(JSON.parse(JSON.stringify(i))))}));P.observe(L,{attributes:!0,subtree:!0,childList:!0})}}}}};exports.fillTextAutoLine=t,exports.watermark=e;
+'use strict';
+
+var getType = function (value) {
+    var s = Object.prototype.toString.call(value);
+    return s.slice(s.indexOf(' ') + 1, s.length - 1);
+};
+
+function fillTextAutoLine(str, canvas, initX, initY, lineHeight) {
+    var ctx = canvas.getContext('2d');
+    if (ctx) {
+        var canvasWidth = canvas.width;
+        var lineWidth = 0;
+        var lastSubStrIndex = 0;
+        for (var i = 0; i < str.length; i++) {
+            lineWidth += ctx.measureText(str[i]).width;
+            if (lineWidth > canvasWidth - initX) {
+                ctx.fillText(str.substring(lastSubStrIndex, i), initX, initY);
+                initY += lineHeight;
+                lineWidth = 0;
+                lastSubStrIndex = i;
+            }
+            if (i === str.length - 1) {
+                ctx.fillText(str.substring(lastSubStrIndex, i + 1), initX, initY);
+            }
+        }
+    }
+}
+
+var watermark = function (options) {
+    var _a = options.container, container = _a === void 0 ? 'body' : _a, _b = options.width, width = _b === void 0 ? '400px' : _b, _c = options.height, height = _c === void 0 ? '300px' : _c, _d = options.content, content = _d === void 0 ? 'mymark' : _d, _e = options.textAlign, textAlign = _e === void 0 ? 'center' : _e, _f = options.textBaseline, textBaseline = _f === void 0 ? 'middle' : _f, _g = options.font, font = _g === void 0 ? '18px Microsoft Yahei' : _g, _h = options.fillStyle, fillStyle = _h === void 0 ? 'rgba(184, 184, 184, 0.3)' : _h, _j = options.lineHeight, lineHeight = _j === void 0 ? 25 : _j, _k = options.rotate, rotate = _k === void 0 ? 20 : _k, _l = options.zIndex, zIndex = _l === void 0 ? 1024 : _l, _m = options.observe, observe = _m === void 0 ? true : _m, _o = options.open, open = _o === void 0 ? true : _o;
+    if (!open)
+        return;
+    var containerEl = typeof container === 'string'
+        ? document.querySelector(container)
+        : container;
+    if (!containerEl)
+        return;
+    var canvas = document.createElement('canvas');
+    canvas.setAttribute('width', width);
+    canvas.setAttribute('height', height);
+    var ctx = canvas.getContext('2d');
+    if (!ctx)
+        return;
+    ctx.textAlign = textAlign;
+    ctx.textBaseline = textBaseline;
+    ctx.font = font;
+    ctx.fillStyle = fillStyle;
+    ctx.rotate((Math.PI / 180) * rotate);
+    var x = parseFloat(width) / 2;
+    var y = parseFloat(height) / 2;
+    if (getType(content) === 'Array') {
+        content.forEach(function (v, i) {
+            fillTextAutoLine(v, canvas, x, y + lineHeight * i, lineHeight);
+        });
+    }
+    else {
+        fillTextAutoLine(content, canvas, x, y, lineHeight);
+    }
+    var base64Url = canvas.toDataURL();
+    var __wm = document.querySelector('.__wm');
+    var watermarkDiv = __wm || document.createElement('div');
+    var styleStr = "\n    position: absolute;\n    top: 0;\n    left: 0;\n    width: 100%;\n    height: 100%; \n    z-index: ".concat(zIndex, ";\n    pointer-events: none;\n    background-repeat: repeat;\n    background-image: url('").concat(base64Url, "')");
+    watermarkDiv.setAttribute('style', styleStr);
+    watermarkDiv.classList.add('__wm');
+    if (!__wm) {
+        containerEl.style.position = 'relative';
+        containerEl.insertBefore(watermarkDiv, containerEl.firstChild);
+    }
+    var MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
+    if (observe && MutationObserver) {
+        var mo_1 = new MutationObserver(function () {
+            var __wm = document.querySelector('.__wm');
+            if ((__wm && __wm.getAttribute('style') !== styleStr) || !__wm) {
+                mo_1.disconnect();
+                watermark(JSON.parse(JSON.stringify(options)));
+            }
+        });
+        mo_1.observe(containerEl, {
+            attributes: true,
+            subtree: true,
+            childList: true,
+        });
+    }
+};
+
+exports.fillTextAutoLine = fillTextAutoLine;
+exports.watermark = watermark;
--- dist/mymark.esm.js
+++ dist/mymark.esm.js
@@ -1,1 +1,84 @@
-function t(t,e,i,n,o){var r=e.getContext("2d");if(r)for(var a=e.width,d=0,l=0,s=0;s<t.length;s++)(d+=r.measureText(t[s]).width)>a-i&&(r.fillText(t.substring(l,s),i,n),n+=o,d=0,l=s),s===t.length-1&&r.fillText(t.substring(l,s+1),i,n)}var e=function(i){var n=i.container,o=void 0===n?"body":n,r=i.width,a=void 0===r?"400px":r,d=i.height,l=void 0===d?"300px":d,s=i.content,c=void 0===s?"mymark":s,v=i.textAlign,u=void 0===v?"center":v,f=i.textBaseline,g=void 0===f?"middle":f,h=i.font,b=void 0===h?"18px Microsoft Yahei":h,p=i.fillStyle,x=void 0===p?"rgba(184, 184, 184, 0.3)":p,m=i.lineHeight,y=void 0===m?25:m,w=i.rotate,S=void 0===w?20:w,A=i.zIndex,O=void 0===A?1024:A,_=i.observe,M=void 0===_||_,k=i.open;if(void 0===k||k){var q="string"==typeof o?document.querySelector(o):o;if(q){var B=document.createElement("canvas");B.setAttribute("width",a),B.setAttribute("height",l);var C=B.getContext("2d");if(C){C.textAlign=u,C.textBaseline=g,C.font=b,C.fillStyle=x,C.rotate(Math.PI/180*S);var E,L,T=parseFloat(a)/2,z=parseFloat(l)/2;"Array"===(E=c,(L=Object.prototype.toString.call(E)).slice(L.indexOf(" ")+1,L.length-1))?c.forEach((function(e,i){t(e,B,T,z+y*i,y)})):t(c,B,T,z,y);var F=B.toDataURL(),I=document.querySelector(".__wm"),J=I||document.createElement("div"),N="\n    position: absolute;\n    top: 0;\n    left: 0;\n    width: 100%;\n    height: 100%; \n    z-index: ".concat(O,";\n    pointer-events: none;\n    background-repeat: repeat;\n    background-image: url('").concat(F,"')");J.setAttribute("style",N),J.classList.add("__wm"),I||(q.style.position="relative",q.insertBefore(J,q.firstChild));var j=window.MutationObserver||window.WebKitMutationObserver;if(M&&j){var D=new j((function(){var t=document.querySelector(".__wm");(t&&t.getAttribute("style")!==N||!t)&&(D.disconnect(),e(JSON.parse(JSON.stringify(i))))}));D.observe(q,{attributes:!0,subtree:!0,childList:!0})}}}}};export{t as fillTextAutoLine,e as watermark};
+var getType = function (value) {
+    var s = Object.prototype.toString.call(value);
+    return s.slice(s.indexOf(' ') + 1, s.length - 1);
+};
+
+function fillTextAutoLine(str, canvas, initX, initY, lineHeight) {
+    var ctx = canvas.getContext('2d');
+    if (ctx) {
+        var canvasWidth = canvas.width;
+        var lineWidth = 0;
+        var lastSubStrIndex = 0;
+        for (var i = 0; i < str.length; i++) {
+            lineWidth += ctx.measureText(str[i]).width;
+            if (lineWidth > canvasWidth - initX) {
+                ctx.fillText(str.substring(lastSubStrIndex, i), initX, initY);
+                initY += lineHeight;
+                lineWidth = 0;
+                lastSubStrIndex = i;
+            }
+            if (i === str.length - 1) {
+                ctx.fillText(str.substring(lastSubStrIndex, i + 1), initX, initY);
+            }
+        }
+    }
+}
+
+var watermark = function (options) {
+    var _a = options.container, container = _a === void 0 ? 'body' : _a, _b = options.width, width = _b === void 0 ? '400px' : _b, _c = options.height, height = _c === void 0 ? '300px' : _c, _d = options.content, content = _d === void 0 ? 'mymark' : _d, _e = options.textAlign, textAlign = _e === void 0 ? 'center' : _e, _f = options.textBaseline, textBaseline = _f === void 0 ? 'middle' : _f, _g = options.font, font = _g === void 0 ? '18px Microsoft Yahei' : _g, _h = options.fillStyle, fillStyle = _h === void 0 ? 'rgba(184, 184, 184, 0.3)' : _h, _j = options.lineHeight, lineHeight = _j === void 0 ? 25 : _j, _k = options.rotate, rotate = _k === void 0 ? 20 : _k, _l = options.zIndex, zIndex = _l === void 0 ? 1024 : _l, _m = options.observe, observe = _m === void 0 ? true : _m, _o = options.open, open = _o === void 0 ? true : _o;
+    if (!open)
+        return;
+    var containerEl = typeof container === 'string'
+        ? document.querySelector(container)
+        : container;
+    if (!containerEl)
+        return;
+    var canvas = document.createElement('canvas');
+    canvas.setAttribute('width', width);
+    canvas.setAttribute('height', height);
+    var ctx = canvas.getContext('2d');
+    if (!ctx)
+        return;
+    ctx.textAlign = textAlign;
+    ctx.textBaseline = textBaseline;
+    ctx.font = font;
+    ctx.fillStyle = fillStyle;
+    ctx.rotate((Math.PI / 180) * rotate);
+    var x = parseFloat(width) / 2;
+    var y = parseFloat(height) / 2;
+    if (getType(content) === 'Array') {
+        content.forEach(function (v, i) {
+            fillTextAutoLine(v, canvas, x, y + lineHeight * i, lineHeight);
+        });
+    }
+    else {
+        fillTextAutoLine(content, canvas, x, y, lineHeight);
+    }
+    var base64Url = canvas.toDataURL();
+    var __wm = document.querySelector('.__wm');
+    var watermarkDiv = __wm || document.createElement('div');
+    var styleStr = "\n    position: absolute;\n    top: 0;\n    left: 0;\n    width: 100%;\n    height: 100%; \n    z-index: ".concat(zIndex, ";\n    pointer-events: none;\n    background-repeat: repeat;\n    background-image: url('").concat(base64Url, "')");
+    watermarkDiv.setAttribute('style', styleStr);
+    watermarkDiv.classList.add('__wm');
+    if (!__wm) {
+        containerEl.style.position = 'relative';
+        containerEl.insertBefore(watermarkDiv, containerEl.firstChild);
+    }
+    var MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
+    if (observe && MutationObserver) {
+        var mo_1 = new MutationObserver(function () {
+            var __wm = document.querySelector('.__wm');
+            if ((__wm && __wm.getAttribute('style') !== styleStr) || !__wm) {
+                mo_1.disconnect();
+                watermark(JSON.parse(JSON.stringify(options)));
+            }
+        });
+        mo_1.observe(containerEl, {
+            attributes: true,
+            subtree: true,
+            childList: true,
+        });
+    }
+};
+
+export { fillTextAutoLine, watermark };
